6b0553b6f768da06df12cae4ee26fd6055ec228c
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-01-17 14:59:07 +0100

    upload file done

--- server.py
+++ server.py
@@ -19,9 +19,13 @@ def home () :
 
 @app.route('/amf', methods=['POST', 'GET'])
 def index () :
-    if request.method == 'POST' and 'amf' in request.files:
-        f = request.files['amf']
-        return jsonify(message='You hit the File upload route of the app')
+    req_file = request.files['amf']
+    if request.method == 'POST' and allowed_file_extensions(req_file.filename):
+        filename = secure_filename(req_file.filename)
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        req_file.save(upload_path)
+        print(upload_path)
+        return jsonify(message='File uploaded successfully' , path=upload_path)
     elif request.method =='POST' and 'amf' not in request.files : 
-        return jsonify (message="You didn't attach amf dat file in the form")
+        return jsonify (message="You didn't attach amf dat file in the form")        
     return jsonify (message='You made a bad request, post request is required')
