cf9920120f3b97c8c111a99c112f841b9cdf30b6
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-01-17 13:45:57 +0100

    file ext check

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 from flask import jsonify, Flask, request
 from werkzeug.utils import secure_filename
+import os
 
 UPLOAD_FOLDER = '/amf'
 ALLOWED_EXTENSIONS = {'dat' '999'}
 
 app = Flask (__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# utility function for checking uploaded files conform to allowed extensions format
+def allowed_file_extensions (filename) : 
+    return '.' in filename and \
+        filename.rsplit('.', 1) [1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def home () :
@@ -19,4 +25,3 @@ def index () :
     elif request.method =='POST' and 'amf' not in request.files : 
         return jsonify (message="You didn't attach amf dat file in the form")
     return jsonify (message='You made a bad request, post request is required')
-    
