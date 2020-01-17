d27eebe939d6c8b4ea3790b64d7af7556bd7488a
Olanrewaju A. Olaboye <36379953+WonderfulOlanrewaju@users.noreply.github.com>
2020-01-17 17:03:16 +0100

    upload succeed

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from flask import jsonify, Flask, request
 from werkzeug.utils import secure_filename
 import os
 
-UPLOAD_FOLDER = '/amf'
-ALLOWED_EXTENSIONS = {'dat' '999'}
+UPLOAD_FOLDER = 'amf'
+ALLOWED_EXTENSIONS = {'dat','999'}
 
 app = Flask (__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -19,13 +19,11 @@ def home () :
 
 @app.route('/amf', methods=['POST', 'GET'])
 def index () :
-    req_file = request.files['amf']
-    if request.method == 'POST' and allowed_file_extensions(req_file.filename):
-        filename = secure_filename(req_file.filename)
-        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        req_file.save(upload_path)
-        print(upload_path)
-        return jsonify(message='File uploaded successfully' , path=upload_path)
+    file = request.files['amf']
+    if request.method == 'POST' and allowed_file_extensions(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify(message='You hit the File upload route of the app')
     elif request.method =='POST' and 'amf' not in request.files : 
-        return jsonify (message="You didn't attach amf dat file in the form")        
+        return jsonify (message="You didn't attach amf dat file in the form")
     return jsonify (message='You made a bad request, post request is required')
